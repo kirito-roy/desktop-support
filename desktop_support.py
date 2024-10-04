a961bcc3505bd780c110444183f178ffd9362305
sumit <roy184433@gmail.com>
2024-10-05 03:07:08 +0530

    added volume button

--- desktop_support.py
+++ desktop_support.py
@@ -34,8 +34,16 @@ class support:
     def wifi_off(self):
         os.system("nmcli radio wifi off")
         os.system('notify-send "Wifi off"')
-    def show_wifi_list():
-        pass
+    def show_wifi_list(self):
+        os.system("nmcli device wifi list")
+        # add function to show wifi list here  # TODO: implement this function
+    def increase_volume(self):
+        os.system("pactl set-sink-volume @DEFAULT_SINK@ +10%")
+    def decrease_volume(self):
+        os.system("pactl set-sink-volume @DEFAULT_SINK@ -10%")
+    def mute(self):
+        os.system("pactl set-sink-mute @DEFAULT_SINK@ toggle")
+
     def run_root(self):
         self.root.mainloop()
 
@@ -63,6 +71,27 @@ class support:
         button7.grid(row=3,column=1)
         button8=Button(self.root,text="both")
         button8.grid(row=3,column=2)
+        button9=Button(self.root,text="mirror")
+        button9.grid(row=3,column=3)
+        label3=Label(text="sound")
+        label3.grid(row=4,column=0)
+        button10=Button(self.root,text="volume +",command=self.increase_volume)  
+        button10.grid(row=5,column=0)
+        button11=Button(self.root,text="volume -",command=self.decrease_volume)
+        button11.grid(row=5,column=1)
+        button12=Button(self.root,text="mute/unmute",command=self.mute)
+
+        button12.grid(row=5,column=2)
+        
+        label4=Label(text="keyboard")
+        label4.grid(row=6,column=0)
+        button13=Button(self.root,text="caps lock")
+        button13.grid(row=7,column=0)
+        button14=Button(self.root,text="num lock")
+        button14.grid(row=7,column=1)
+        button15=Button(self.root,text="scroll lock")
+        button15.grid(row=7,column=2)
+
         
 
 
